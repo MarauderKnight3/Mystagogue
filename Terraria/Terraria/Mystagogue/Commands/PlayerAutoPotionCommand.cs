e50eaa874255b1710a3b22a44f1ad9976e381da2
MarauderKnight3 <marauderknight3@gmail.com>
2025-08-15 02:38:04 -0500

    auto potion command

---
+++ Terraria/Terraria/Mystagogue/Commands/PlayerAutoPotionCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Terraria.Mystagogue.Commands;
+internal class PlayerAutoPotionCommand : Command
+{
+	internal static bool AutoPotion;
+
+	public PlayerAutoPotionCommand() : base("ap", "Enables or disables whether your character quick buffs on their own.") { }
+	protected internal override void Execute(List<string> args)
+	{
+		AutoPotion = !AutoPotion;
+		Output("God mode toggled " + (AutoPotion ? "on" : "off") + ".");
+		Main.player[Main.myPlayer].QuickBuff();
+	}
+
+	protected internal override void ResetVariables() => AutoPotion = false;
+}
