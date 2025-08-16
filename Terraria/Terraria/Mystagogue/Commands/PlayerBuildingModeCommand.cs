260b85cc38b83e6a9c76baf984c2731672a43ffe
MarauderKnight3 <marauderknight3@gmail.com>
2025-08-16 10:34:31 -0500

    Further changes to building mode

--- Terraria/Terraria/Mystagogue/Commands/PlayerBuildingModeCommand.cs
+++ Terraria/Terraria/Mystagogue/Commands/PlayerBuildingModeCommand.cs
@@ -11,6 +11,12 @@ internal class PlayerBuildingModeCommand : Command
 	{
 		BuildingMode = !BuildingMode;
 		Output("Building mode toggled " + (BuildingMode ? "on" : "off") + ".");
+		if (!BuildingMode)
+			for (int i = 0; i < Main.player[Main.myPlayer].inventory.Length; i++)
+				if (Main.player[Main.myPlayer].inventory[i].MystagogueBuildingModeModified) {
+					Main.player[Main.myPlayer].inventory[i].MystagogueBuildingModeModified = false;
+					Main.player[Main.myPlayer].inventory[i].Refresh();
+				}
 	}
 
 	protected internal override void ResetVariables() => BuildingMode = false;
@@ -20,23 +26,27 @@ internal class PlayerBuildingModeCommand : Command
 		if (!BuildingMode)
 			return;
 
-		foreach (Item item in new List<Item>() { Main.player[Main.myPlayer].HeldItem, Main.mouseItem }) {
-			if (item.IsAir || item.tileBoost == 10000)
+		for (int i = 0; i < Main.player[Main.myPlayer].inventory.Length; i++) {
+			ref Item item = ref Main.player[Main.myPlayer].inventory[i];
+
+			if (item.IsAir || item.MystagogueBuildingModeModified)
 				continue;
 
-			if (item.createTile != -1 || item.createWall != -1 || item.pick != 0 || item.axe != 0 || item.hammer != 0 || item.PaintOrCoating || item.mech) {
+			if (item.createTile != -1 || item.createWall != -1 || item.pick != 0 || item.axe != 0 || item.hammer != 0 || item.PaintOrCoating || item.mech || ItemID.Sets.AlsoABuildingItem[item.type]) {
 				item.useTime = 0;
 				item.tileBoost = 10000;
+
+				if (item.pick > 0)
+					item.pick = ContentSamples.ItemsByType[ItemID.VortexPickaxe].pick * 10;
+
+				if (item.axe > 0)
+					item.axe = ContentSamples.ItemsByType[ItemID.TheAxe].axe * 10;
+
+				if (item.hammer > 0)
+					item.hammer = ContentSamples.ItemsByType[ItemID.TheAxe].hammer * 10;
+
+				item.MystagogueBuildingModeModified = true;
 			}
-
-			if (item.pick > 0)
-				item.pick = ContentSamples.ItemsByType[ItemID.VortexPickaxe].pick * 10;
-
-			if (item.axe > 0)
-				item.axe = ContentSamples.ItemsByType[ItemID.TheAxe].axe * 10;
-
-			if (item.hammer > 0)
-				item.hammer = ContentSamples.ItemsByType[ItemID.TheAxe].hammer * 10;
 		}
 	}
 }
