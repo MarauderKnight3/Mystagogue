d88558a36ed95bd6b42ad94b6c2d74efa6091685
MarauderKnight3 <marauderknight3@gmail.com>
2025-08-16 12:14:33 -0500

    More building mode fixes

--- Terraria/Terraria/Mystagogue/Commands/PlayerBuildingModeCommand.cs
+++ Terraria/Terraria/Mystagogue/Commands/PlayerBuildingModeCommand.cs
@@ -11,12 +11,6 @@ internal class PlayerBuildingModeCommand : Command
 	{
 		BuildingMode = !BuildingMode;
 		Output("Building mode toggled " + (BuildingMode ? "on" : "off") + ".");
-		if (!BuildingMode)
-			for (int i = 0; i < Main.player[Main.myPlayer].inventory.Length; i++)
-				if (Main.player[Main.myPlayer].inventory[i].MystagogueBuildingModeModified) {
-					Main.player[Main.myPlayer].inventory[i].MystagogueBuildingModeModified = false;
-					Main.player[Main.myPlayer].inventory[i].Refresh();
-				}
 	}
 
 	protected internal override void ResetVariables() => BuildingMode = false;
@@ -29,12 +23,12 @@ internal class PlayerBuildingModeCommand : Command
 		for (int i = 0; i < Main.player[Main.myPlayer].inventory.Length; i++) {
 			ref Item item = ref Main.player[Main.myPlayer].inventory[i];
 
-			if (item.IsAir || item.MystagogueBuildingModeModified)
+			if (item.IsAir)
 				continue;
 
 			if (item.createTile != -1 || item.createWall != -1 || item.pick != 0 || item.axe != 0 || item.hammer != 0 || item.PaintOrCoating || item.mech || ItemID.Sets.AlsoABuildingItem[item.type]) {
 				item.useTime = 0;
-				item.tileBoost = 10000;
+				item.tileBoost = 70;
 
 				if (item.pick > 0)
 					item.pick = ContentSamples.ItemsByType[ItemID.VortexPickaxe].pick * 10;
@@ -44,8 +38,6 @@ internal class PlayerBuildingModeCommand : Command
 
 				if (item.hammer > 0)
 					item.hammer = ContentSamples.ItemsByType[ItemID.TheAxe].hammer * 10;
-
-				item.MystagogueBuildingModeModified = true;
 			}
 		}
 	}
