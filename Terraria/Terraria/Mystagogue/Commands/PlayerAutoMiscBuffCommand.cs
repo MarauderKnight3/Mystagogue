4e8eefcb74e8e26083aa435ad5a1d155533164a4
MarauderKnight3 <marauderknight3@gmail.com>
2025-08-24 20:03:53 -0500

    auto misc buff command

---
+++ Terraria/Terraria/Mystagogue/Commands/PlayerAutoMiscBuffCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Terraria.Mystagogue.Commands;
+internal class PlayerAutoMiscBuffCommand : Command
+{
+	internal static bool AutoMiscBuff;
+
+	public PlayerAutoMiscBuffCommand() : base("misc", "Enables or disables whether your character gains a collection of environmental and table buffs automatically.") { }
+	protected internal override void Execute(List<string> args)
+	{
+		AutoMiscBuff = !AutoMiscBuff;
+		Output("Auto misc buffs toggled " + (AutoMiscBuff ? "on" : "off") + ".");
+	}
+
+	protected internal override void ResetVariables() => AutoMiscBuff = false;
+}
