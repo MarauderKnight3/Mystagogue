42f9f041c7e3e39a49435e5e976a3bee7eeb6304
MarauderKnight3 <marauderknight3@gmail.com>
2025-08-25 21:40:27 -0500

    Reforge command edits

--- Terraria/Terraria/Mystagogue/Commands/ItemPrefixCommand.cs
+++ Terraria/Terraria/Mystagogue/Commands/ItemPrefixCommand.cs
@@ -4,19 +4,24 @@ using Terraria.Mystagogue.Utils;
 namespace Terraria.Mystagogue.Commands;
 internal class ItemPrefixCommand : Command
 {
-	public ItemPrefixCommand() : base("reforge", "[Item Prefix/ID] The item held in your cursor will have this prefix.") { }
+	public ItemPrefixCommand() : base("reforge", "[Item Prefix/ID] The item held in the cursor, or in the hotbar if the cursor is empty, will have this prefix.") { }
 	protected internal override void Execute(List<string> args)
 	{
 		// The player must be holding an item to change it.
-		if (Main.player[Main.myPlayer].HeldItem.IsAir) {
+		if (Main.mouseItem.IsAir && Main.player[Main.myPlayer].HeldItem.IsAir) {
 			Output("You aren't holding an item to change.", true);
 			return;
 		}
 
+		// Switch to the hotbar if the cursor is empty.
+		ref var item = ref Main.mouseItem;
+		if (Main.mouseItem.IsAir)
+			item = Main.player[Main.myPlayer].HeldItem;
+
 		byte prefixID = 0;
 		// Interpret the prefix ID or name
 		if (args.Count > 0) {
-			switch (PrefixHelper.NameOrIDToID(args[0], Main.mouseItem.type)) {
+			switch (PrefixHelper.NameOrIDToID(args[0], item.type)) {
 				case string errorReason:
 					Output(errorReason, true);
 					return;
@@ -27,8 +32,8 @@ internal class ItemPrefixCommand : Command
 		}
 
 		// Edit the mouse item
-		Main.mouseItem.prefix = prefixID;
-		Main.mouseItem.Refresh();
+		item.prefix = prefixID;
+		item.Refresh();
 
 		// Let the user know
 		Output($"Reforged to [{PrefixHelper.PrefixNames[prefixID]}].");
